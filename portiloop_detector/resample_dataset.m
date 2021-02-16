c20f55465b60780497507e829309c508f31bbcde
Nicolas <nicolas.valenchon@polymtl.ca>
2021-02-16 12:11:21 -0500

    add resample_dataset (matlab file)

---
+++ portiloop_detector/resample_dataset.m
@@ -0,0 +1,13 @@
+%% load data
+path = "../dataset/";
+spindle_250 = load(path+"spindles_annotations_at_250hz.txt");
+data_256 = load(path+"data_to_resample.txt");
+%% resample
+data_250_matlab = resample(data_256, 250, 256);
+%% create vector
+size_250 = size(spindle_250,1);
+time_vect_250 = linspace(0,size_250/250, size_250);
+%% generate vector
+data_250 = [data_250_matlab, spindle_250];
+%% create file
+writematrix(spindle_250, path+"dataset_small_250_matlab.txt");
