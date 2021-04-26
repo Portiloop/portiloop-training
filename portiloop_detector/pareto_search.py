5057e6234e843ba061d81191dda293fe8c10b038
Nicolas <nicolas.valenchon@polymtl.ca>
2021-04-25 22:14:25 -0400

    increase number of parameter surogate network

--- portiloop_detector/pareto_search.py
+++ portiloop_detector/pareto_search.py
@@ -366,16 +366,16 @@ class SurrogateModel(nn.Module):
         super(SurrogateModel, self).__init__()
 
         self.fc1 = nn.Linear(in_features=13,  # nb hyperparameters
-                             out_features=200)  # in SMBO paper : 25 * hyperparameters... Seems huge
+                             out_features=13*25)  # in SMBO paper : 25 * hyperparameters... Seems huge
 
         self.d1 = nn.Dropout(0.5)
 
-        self.fc2 = nn.Linear(in_features=200,
-                             out_features=200)
+        self.fc2 = nn.Linear(in_features=13*25,
+                             out_features=13*25)
 
         self.d2 = nn.Dropout(0.5)
 
-        self.fc3 = nn.Linear(in_features=200,
+        self.fc3 = nn.Linear(in_features=13*25,
                              out_features=1)
 
     def to(self, device):
