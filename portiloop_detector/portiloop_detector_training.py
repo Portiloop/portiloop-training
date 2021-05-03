4488d039ac61a47cbab7e5d70a738dc1893b2a30
Nicolas <nicolas.valenchon@polymtl.ca>
2021-05-03 12:07:05 -0400

    test

--- portiloop_detector/portiloop_detector_training.py
+++ portiloop_detector/portiloop_detector_training.py
@@ -726,7 +726,7 @@ def run(config_dict):
             best_model_loss_validation = loss_validation
             best_model_accuracy = accuracy_validation
 
-        loss_early_stopping = max(0.5, loss_validation) if loss_early_stopping is None else loss_validation * early_stopping_smoothing_factor + loss_early_stopping * (
+        loss_early_stopping = max(0.2, loss_validation) if loss_early_stopping is None else loss_validation * early_stopping_smoothing_factor + loss_early_stopping * (
                 1.0 - early_stopping_smoothing_factor)
 
         if loss_early_stopping < best_loss_early_stopping:
