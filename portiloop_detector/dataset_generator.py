13f820d794af66b9ff4a551c6e89925f558c7f08
Nicolas <nicolas.valenchon@polymtl.ca>
2021-05-18 14:41:17 -0400

    generate dataset for p2 subject

--- portiloop_detector/dataset_generator.py
+++ portiloop_detector/dataset_generator.py
@@ -13,13 +13,16 @@ import numpy as np
 import pandas as pd
 import pyedflib
 
+phase = 'p2'
+subject_file = {'p1': "6_segListSrcDataLoc_p1.txt", 'p2': "7_segListSrcDataLoc_p2"}
+
 t_start = time()
 path_dataset = Path(__file__).absolute().parent.parent / 'dataset'
 os.chdir(path_dataset)
-p1_list = pd.read_csv("6_segListSrcDataLoc_p1.txt", delim_whitespace=True)
-p1_subject_list = list(dict.fromkeys(p1_list["subjectID"]))
+phase_list = pd.read_csv(subject_file[phase], delim_whitespace=True)
+phase_subject_list = list(dict.fromkeys(phase_list["subjectID"]))
 annotation_files = glob.glob("*MODA_GS.txt")
-annotation_files = [files for files in annotation_files if files[0:-12] in p1_subject_list]
+annotation_files = [files for files in annotation_files if files[0:-12] in phase_subject_list]
 signal_files = [(subject[0:-12] + " PSG.edf") for subject in annotation_files]
 fe = 256
 new_fe = 250
@@ -94,11 +97,10 @@ signal = np.hstack(np.hstack(signal_seq_list))
 spindle = np.hstack(np.hstack(spindle_seq_list))
 
 if fe == new_fe:
-    np.savetxt("dataset_p1_" + size_dataset + ".txt", np.transpose((signal, spindle)), fmt='%e,%f')
+    np.savetxt(f"dataset_{phase}_{size_dataset}.txt", np.transpose((signal, spindle)), fmt='%e,%f')
 else:
-    np.savetxt(f"dataset_p1_{size_dataset}_at_{fe}_to_resample.txt", np.transpose(signal), fmt='%e')
-    np.savetxt(f"spindles_annotations_p1_{size_dataset}_at_{new_fe}hz.txt", np.transpose(spindle), fmt='%f')
+    np.savetxt(f"dataset_{phase}_{size_dataset}_at_{fe}_to_resample.txt", np.transpose(signal), fmt='%e')
+    np.savetxt(f"spindles_annotations_{phase}_{size_dataset}_at_{new_fe}hz.txt", np.transpose(spindle), fmt='%f')
 
-
-np.savetxt(f"subject_sequence_p1_{size_dataset}.txt", subject_seq_list, fmt="%s")
+np.savetxt(f"subject_sequence_{phase}_{size_dataset}.txt", subject_seq_list, fmt="%s")
 print("tot_time = ", str(time() - t_start))
