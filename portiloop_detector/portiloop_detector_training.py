fd4cb955952e991004ade26e2582f1deb409535b
Nicolas <nicolas.valenchon@polymtl.ca>
2021-05-18 18:20:37 -0400

    move a print that was too early

--- portiloop_detector/portiloop_detector_training.py
+++ portiloop_detector/portiloop_detector_training.py
@@ -690,8 +690,8 @@ def run(config_dict, wandb_project, save_model, unique_name):
     first_epoch = 0
     try:
         logger.restore()
-        print("DEBUG: Use checkpoint model")
         checkpoint = torch.load(path_dataset / experiment_name)
+        print("DEBUG: Use checkpoint model")
         net.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         first_epoch = checkpoint['epoch'] + 1
