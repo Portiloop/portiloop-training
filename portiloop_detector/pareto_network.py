5c283573f83aa53944a45b9c932e6a03d8cb57ef
Nicolas <nicolas.valenchon@polymtl.ca>
2021-05-19 14:18:21 -0400

    change file name

--- portiloop_detector/pareto_network.py
+++ portiloop_detector/pareto_network.py
@@ -13,7 +13,7 @@ from requests import get
 from pareto_network_server_utils import Server, RECV_TIMEOUT_META_FROM_SERVER, SOCKET_TIMEOUT_CONNECT_META, PORT_META, LOOP_SLEEP_TIME, RECV_TIMEOUT_WORKER_FROM_SERVER, \
     PORT_WORKER, SOCKET_TIMEOUT_CONNECT_WORKER, ACK_TIMEOUT_WORKER_TO_SERVER, IP_SERVER, ACK_TIMEOUT_META_TO_SERVER, select_and_send_or_close_socket, print_with_timestamp, poll_and_recv_or_close_socket, get_connected_socket
 from pareto_search import LoggerWandbPareto, RUN_NAME, SurrogateModel, META_MODEL_DEVICE, train_surrogate, update_pareto, nb_parameters, MAX_NB_PARAMETERS, NB_SAMPLED_MODELS_PER_ITERATION, exp_max_pareto_efficiency, run, \
-    load_network_files, dump_network_files, transform_config_dict_to_input, WANDB_PROJECT_PARETO, PARETO_ID
+    load_network_files, dump_network_files, transform_config_dict_to_input, WANDB_PROJECT_PARETO, PARETO_ID, path_dataset
 from utils import same_config_dict, sample_config_dict, MIN_NB_PARAMETERS, MAXIMIZE_F1_SCORE, PROFILE_META
 
 
@@ -319,7 +319,7 @@ class Worker:
                 self.__exp_to_run = None
                 self.__exp_to_run_lock.release()
                 original_stdout = sys.stdout  # Save a reference to the original standard output
-                with open("test_output_.txt", 'a') as f:
+                with open(path_dataset / f"{exp['config_dict']['experiment_name']}.txt", 'a') as f:
                     sys.stdout = f  # Change the standard output to the file we created.
                     predicted_loss = exp['cost_software']
                     best_loss, best_f1_score, exp["best_epoch"] = run(exp["config_dict"], f"{WANDB_PROJECT_PARETO}_runs_{PARETO_ID}", save_model=False, unique_name=True)
