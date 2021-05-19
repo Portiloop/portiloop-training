5fccaa617e8d4ac7d09fb79f86775f083186f3b2
Nicolas <nicolas.valenchon@polymtl.ca>
2021-05-19 14:37:47 -0400

    debug

--- portiloop_detector/utils.py
+++ portiloop_detector/utils.py
@@ -3,7 +3,7 @@ from math import floor
 from random import choices, uniform, gauss
 
 import logging
-logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
 logging.debug('This message should go to the log file')
 logging.info('So should this')
 logging.warning('And this, too')
