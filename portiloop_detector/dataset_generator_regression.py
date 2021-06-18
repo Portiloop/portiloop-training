f90850f80b6eb664d4168dd3b258bcbc09f38335
Nicolas <nicolas.valenchon@polymtl.ca>
2021-06-17 23:05:13 -0400

    debug

--- portiloop_detector/dataset_generator_regression.py
+++ portiloop_detector/dataset_generator_regression.py
@@ -88,7 +88,7 @@ def generate(phase):
                 print("sequence too early")
                 continue
             endIdx = int(endSeq * new_fe) + post_sequence_length
-            lenSignal = (post_sequence_length+pre_sequence_length + 115)*new_fe
+            lenSignal = post_sequence_length+pre_sequence_length + 115*new_fe
             signal_seq_list[i][index] = signal_list[i][int(startIdx * fe / new_fe):int(endIdx * fe / new_fe)]
             spindle_seq_list[i][index] = np.zeros((lenSignal,), dtype=float)
             spindle_seq_list[i][index][:pre_sequence_length] = -1
